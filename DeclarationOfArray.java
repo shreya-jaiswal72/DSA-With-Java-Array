00f7559e519d89652046fc4b4526ad3e833d7c5d
shreya-jaiswal72 <professional121409@gmail.com>
2024-09-21 10:48:58 -0800

    Adding How to Create and Initalize an Array

--- DeclarationOfArray.java
+++ DeclarationOfArray.java
@@ -2,8 +2,47 @@ public class DeclarationOfArray
 {
     public static void main(String[] args)
     {
-        int[] a;    //recommended
-        int []a;
+        //Declaration of an Array
+        // int[] a;    //recommended
+        // int []a;
+        // int a[];
+
+
+        //Creating an Array
+        // a=new int[5];
+
+        // initializing an Array
+        // System.out.println(a[0]);        //0
+
+
         int a[];
+        a=new int[2];
+        System.out.println(a[0]); 
+        System.out.println(a[1]); 
+        
+
+        boolean b[];
+        b=new boolean[2];
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+        
+        float f[];
+        f=new float[2];
+        System.out.println(f[0]);
+        System.out.println(f[1]);
+
+        String s[];
+        s=new String[2];
+        System.out.println(s[0]);
+        System.out.println(s[1]);
+
+        char c[];
+        c=new char[2];
+        System.out.println(c[0]);
+        System.out.println(c[1]);
+
+
+        
+
     }
 }
