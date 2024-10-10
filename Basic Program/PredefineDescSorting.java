d54247a70781c2755ce687daf0afaf257ae318c6
shreya-jaiswal72 <professional121409@gmail.com>
2024-10-10 02:14:23 -0800

    Predefined methods of sorting in Desccending order.

---
+++ Basic Program/PredefineDescSorting.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PredefineDescSorting {
+     public static void main(String[] args)
+{
+	Scanner obj=new Scanner(System.in);
+	System.out.println("Enter the size of an array: ");
+	int n = obj.nextInt();
+	int a[]=new int[n];
+	int i;
+	System.out.println("Enter the " +n+" element in an array");
+	for(i=0;i<n;i++)
+	{
+		a[i]=obj.nextInt();
+	}
+	
+	System.out.println("Before Sorting");
+	for(i=0;i<n;i++)
+		System.out.print(a[i]+ " ");
+	System.out.println();
+	
+	
+	//logic
+	Arrays.sort(a);
+	
+	System.out.println("After Sorting");
+	for(i=n-1;i>=0;i--)
+	{
+		System.out.print(a[i]+" ");
+	}
+}
+}
