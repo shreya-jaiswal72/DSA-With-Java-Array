1fa117c599adae2ef3708e912d6837e86bd211e2
shreya-jaiswal72 <professional121409@gmail.com>
2024-10-10 03:35:42 -0800

    Program to sort the character

---
+++ Basic Program/CharacterSorting.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CharacterSorting {
+    
+    public static void main(String[] args)
+	{
+		Scanner obj=new Scanner(System.in);
+
+		System.out.println("Enter the size of an array: ");
+		String s=obj.nextLine();
+		char t,a[]=s.toCharArray();
+		for(int i=0;i<a.length;i++)
+        {
+		    for(int j=i+1;j<a.length;j++)
+		    {
+			    if(a[i]>a[j])
+			    {
+			    	t=a[i];
+			    	a[i]=a[j];
+			    	a[j]=t;
+			    }
+		    }
+        }
+		
+		System.out.println(new String(a));
+	}
+}
