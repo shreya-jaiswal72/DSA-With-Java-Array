c1c217b99818bc7a63959d01c8a473ce5e0de255
shreya-jaiswal72 <professional121409@gmail.com>
2024-10-17 08:08:57 -0800

    This Program will read one-by-one from array and it converts into the following string format.

---
+++ Basic Program/ToString.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class ToString {
+    public static void main(String[] args)
+    {
+        int a[]={10,20,30,40,50};
+        System.out.println(Arrays.toString(a));
+    }
+}
