7846288220b8ec6d0417165f0b010f22a357bf41
shreya-jaiswal72 <professional121409@gmail.com>
2024-10-25 11:51:27 -0800

    Program to calculate transpose of the given matrix.

---
+++ Matrix/TransposeMatrix.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class TransposeMatrix {
+    public static void main(String[] args) {
+         Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the row size:");
+        int rsize=obj.nextInt();
+        System.out.println("Enter the column size:");
+        int csize=obj.nextInt();
+        int i,j;
+        int a[][]=new int[rsize][csize];
+        System.out.println("Enter the elements in the matrix:");
+        for(i=0;i<rsize;i++)
+        {
+            for(j=0;j<csize;j++)
+            {
+                a[i][j]=obj.nextInt();
+            }
+        }
+        for(i=0;i<rsize;i++)
+        {
+            for(j=0;j<csize;j++)
+            {
+                System.out.print(a[j][i]+" ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
