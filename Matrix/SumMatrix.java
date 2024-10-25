3b43ab06d27b3fe05075648e5b2362764c41f7e1
shreya-jaiswal72 <professional121409@gmail.com>
2024-10-24 23:43:55 -0800

    Program to read and calculate sum of all elements present in the matrix.

---
+++ Matrix/SumMatrix.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SumMatrix {
+    public static void main(String []args)
+    {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the row size:");
+        int rsize=obj.nextInt();
+        System.out.println("Enter the column size:");
+        int csize=obj.nextInt();
+
+        int i,j,sum=0;
+        int a[][]=new int[rsize][csize];
+        System.out.println("Enter the elements in the matrix:");
+        for(i=0;i<rsize;i++)
+        {
+            for(j=0;j<csize;j++)
+            {
+                a[i][j]=obj.nextInt();
+            }
+        }
+
+        for(i=0;i<rsize;i++)
+        {
+            for(j=0;j<csize;j++)
+            {
+                sum=sum + a[i][j];
+            }
+        }
+        System.out.println("Sum of elements in the matrix = "+sum);
+    }
+}
