cf0cd86211ed3402385d74224767d33cee3f6c78
shreya-jaiswal72 <professional121409@gmail.com>
2024-10-26 04:28:46 -0800

    Program to interchanging of diagonal values in the matrix.

--- Matrix/InterchangingOfDiagonal.java
+++ Matrix/InterchangingOfDiagonal.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
-public class SumOfOppositeDiagonalValues {
-    public static void main(String[] args) {
-
+public class InterchangingOfDiagonal {
+        public static void main(String[] args) {
         Scanner obj=new Scanner(System.in);
         System.out.println("Enter the row size:");
         int rsize=obj.nextInt();
         System.out.println("Enter the column size:");
         int csize=obj.nextInt();
-        int i,j,sum=0;
+        int i,j,t;
         int a[][]=new int[rsize][csize];
         System.out.println("Enter the elements in the matrix:");
         for(i=0;i<rsize;i++)
@@ -18,14 +17,32 @@ public class SumOfOppositeDiagonalValues {
                 a[i][j]=obj.nextInt();
             }
         }   
+        System.out.println("Before Swapping: ");
         for(i=0;i<rsize;i++)
         {
             for(j=0;j<csize;j++)
             {
-                sum=sum+a[i][rsize-i-1];
+                System.out.print(a[i][j]+" ");
             }
+            System.out.println();
         }
-        System.out.println("Sum of Diagonal elements in the matrix = "+sum);
 
+        for(i=0;i<rsize;i++)
+        {
+            t=a[i][i];
+            a[i][i]=a[i][rsize-i-1];
+            a[i][rsize-i-1]=t;
+        }
+
+        System.out.println("After Swapping: ");
+        for(i=0;i<rsize;i++)
+        {
+            for(j=0;j<csize;j++)
+            {
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+        
     }
 }
