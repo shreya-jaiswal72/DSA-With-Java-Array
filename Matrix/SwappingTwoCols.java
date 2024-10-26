b6773fe80f6905d0679a58e304dff3c2a1d7564d
shreya-jaiswal72 <professional121409@gmail.com>
2024-10-26 03:21:30 -0800

    Program to Swap of two columns.

---
+++ Matrix/SwappingTwoCols.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class SwappingTwoCols {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the row value:");
+        int row=obj.nextInt();
+        System.out.println("Enter the column value:");
+        int col=obj.nextInt();
+        int i,j,n,m,t;
+        int a[][]=new int[row][col];
+        System.out.println("Enter the elements in the matrix:");
+        for(i=0;i<row;i++)
+        {
+            for(j=0;j<col;j++)
+            {
+                a[i][j]=obj.nextInt();
+            }
+        }
+            
+        System.out.println("Enter the n and m value:");
+         n=obj.nextInt();
+         m=obj.nextInt();
+         System.out.println("Before Swapping: ");
+        for(i=0;i<row;i++)
+        {
+            for(j=0;j<col;j++)
+            {
+                    System.out.print(a[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        for(i=0;i<col;i++)
+        {
+            t=a[i][m-1];
+            a[i][m-1]= a[i][n-1];
+            a[i][n-1]=t;    
+        }
+        System.out.println("After Swapping:");
+        for(i=0;i<row;i++)
+        {
+            for(j=0;j<col;j++)
+            {
+                    System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+    
+    
+    
+    
+    }
+}
